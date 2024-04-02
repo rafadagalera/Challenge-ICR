474969154fc2dcf3e9b641f99c330dff0982ce1a
Rafael Nascimento <rm553117@fiap.com.br~>
2024-04-02 01:00:30 -0300

    mudança main

--- Computational Thinking With Python/main.py
+++ Computational Thinking With Python/main.py
@@ -1,24 +1,3 @@
 from funcoes import menu
 
-operacoes = []
-
-email_logado = []
-
-senha_logada = []
-
-cadastros = {
-    "Nome":[],
-    "Email":[],
-    "Senha":[],
-    "Avatar":[]
-}
-
-caractericas_avatar = {
-    "Cabelo": "",
-    "Rosto": "",
-    "Camisa": "",
-    "Short": "",
-    "Tenis": ""
-}
-
 menu()
