dc5be4101f090b770d0210df89a2fb72e041a903
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 11:48:56 -0300

    atualizacao da estilizacao e componentes

--- front-web-dev/src/components/Header/index.jsx
+++ front-web-dev/src/components/Header/index.jsx
@@ -1,25 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import amigosSaudeLogo from "../../assets/amigos-saude-logo.png"
 import "./style.css";
 
 const Header = () => {
+    const navigate = useNavigate()
+
     return (
         <header className="container">
-            <h1>logo</h1>
+            <img src={amigosSaudeLogo} alt="" />
             <nav>
                 <ul>
                     <li>
-                        <p>Home</p>
+                        <p onClick={() => navigate('/')}>Home</p>
                     </li>
                     <li>
-                        <p>Criar meu personagem</p>
+                        <p onClick={() => navigate('/create-avatar')}>Criar meu personagem</p>
                     </li>
                     <li>
-                        <p>Meu perfil</p>
+                        <p onClick={() => navigate('/')}>Amigos da saúde</p>
                     </li>
                     <li>
-                        <p>Amigos da saúde</p>
+                        <p onClick={() => navigate('/login')}>Meu perfil</p>
                     </li>
                     <li>
-                        <p>Ajuda</p>
+                        <p onClick={() => navigate('/')}>Ajuda</p>
                     </li>
                 </ul>
             </nav>
--- front-web-dev/src/routes/index.jsx
+++ front-web-dev/src/routes/index.jsx
@@ -3,6 +3,8 @@ import { Register } from "../pages/register"
 import { Login } from "../pages/login"
 import { MainPage } from "../pages/main-page"
 import { CreateAvatar } from "../pages/create-avatar"
+import { AmigosSaude } from "../pages/amigos-saude"
+import { Ajuda } from "../pages/ajuda"
 
 export const RoutesMain = () => {
     return (
@@ -11,6 +13,8 @@ export const RoutesMain = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/create-avatar" element={<CreateAvatar />} />
+            <Route path="/amigos-saude" element={<AmigosSaude />} />
+            <Route path="/ajuda" element={<Ajuda />} />
         </Routes>
     )
 }
