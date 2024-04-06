27efc4c6ec12b2be222acdfb3ee433acd34b2e03
Rafael Nascimento <rm553117@fiap.com.br~>
2024-04-05 21:59:59 -0300

    adicionando link no botão

--- front-web-dev/src/pages/main-page/index.jsx
+++ front-web-dev/src/pages/main-page/index.jsx
@@ -11,7 +11,7 @@ export const MainPage = () => {
                 <HomeContainer>
                         <h1 className='titulo'>Bem vindo ao nosso site!</h1>
                         <p className='text'>Bem-vindo ao nosso mundo de aprendizado hospitalar interativo! Aqui, pacientes infantis criam avatares para explorar experiências educativas únicas. Nossa plataforma oferece uma jornada lúdica e informativa pelos processos hospitalares, ajudando a desmistificar e confortar. Junte-se a nós enquanto transformamos aprendizado em aventura, garantindo que cada passo seja guiado por diversão e entendimento. Seja bem-vindo ao universo onde o conhecimento é a melhor medicina!</p>
-                        <button className='create_avatar'>Criar meu personagem</button>
+                        <a href="/create-avatar"><button className='create_avatar'>Criar meu personagem</button></a>
                 </HomeContainer>
             </StyledDiv>
             <Footer />
