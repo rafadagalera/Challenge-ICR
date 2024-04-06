3e6bec560a82a8ba0639c52d3245795024f78622
Luis Alberto <luisalberto-rocha@hotmail.com>
2024-04-05 21:06:24 -0300

    finalizado criacao de personagem

--- front-web-dev/src/pages/create-avatar/index.jsx
+++ front-web-dev/src/pages/create-avatar/index.jsx
@@ -1,15 +1,42 @@
-import { Footer } from '../../components/Footer'
-import { Header } from '../../components/Header'
-import { StyledDiv } from './style'
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { AvatarDiv, StyledDiv } from "./style";
+import avatar from "../../assets/Screenshot_2.png";
 
 export const CreateAvatar = () => {
-    return(
+    return (
         <>
-            <Header />
-            
-            <StyledDiv />
+            <Header />1
+            <StyledDiv className="container">
+                <AvatarDiv>
+                    <img src={avatar} alt="" />
+                    <form>
+                        <div>
+                            <label htmlFor="nome">Nome</label>
+                            <input type="text" id="nome" />
+                        </div>
+                        <div>
+                            <label htmlFor="idade">Idade</label>
+                            <input type="text" id="idade" />
+                        </div>
+                        <div>
+                            <label htmlFor="filme">Filme favorito</label>
+                            <input type="text" id="filme" />
+                        </div>
 
+                        <textarea
+                            name="sobre_voce"
+                            id=""
+                            cols="50"
+                            rows="10"
+                            placeholder="Escreva aqui..."
+                        ></textarea>
+
+                        <button>Finalizar</button>
+                    </form>
+                </AvatarDiv>
+            </StyledDiv>
             <Footer />
         </>
-    )
-}
+    );
+};
--- front-web-dev/src/pages/create-avatar/style.jsx
+++ front-web-dev/src/pages/create-avatar/style.jsx
@@ -1,5 +1,63 @@
 import { styled } from "styled-components";
 
 export const StyledDiv = styled.div`
-    
+    display: flex;
+    justify-content: center;
+
+    margin-top: 50px;
+    margin-bottom: 100px;
+`;
+
+export const AvatarDiv = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5rem;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        width: 40%;
+    }
+
+    form div {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+
+    form div input {
+        width: 100%;
+        border: 1px solid;
+        border-radius: 5px;
+
+        height: 25px;
+
+        padding: 0.625rem;
+    }
+
+    button {
+        width: 100%;
+        height: 3.5rem;
+        border: 1px solid black;
+        border-radius: 5px;
+
+        font-size: 1.3rem;
+        font-weight: 500;
+        letter-spacing: 0.15rem;
+
+        background: none;
+    }
+
+    button:hover{
+        background-color: lightgray;
+    }
+
+    @media (max-width: 1200px) {
+        flex-direction: column;
+
+        form {
+            width: 100%;
+        }
+    }
 `;
